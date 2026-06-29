8c98de4cb8ee5cf6d3764b4d7348c61ffffa6045
Uttam Makwana <uttamakwana4503@gmail.com>
2026-06-30 00:07:19 +0530

    feat: created types of product and category

---
+++ src/features/products/types.ts
@@ -0,0 +1,51 @@
+export interface IProductDimensions {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+export interface IProductReview {
+    rating: number;
+    comment: string;
+    date: Date;
+    reviewerName: string;
+    reviewerEmail: string;
+}
+
+export interface IProductMeta {
+    createdAt: Date;
+    updatedAt: Date;
+    barcode: string;
+    qrCode: string;
+}
+
+export interface IProduct {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    tags: string[],
+    brand: string;
+    sku: string;
+    weight: number;
+    dimensions: IProductDimensions;
+    warrantyInformation: string;
+    shippingInformation: string;
+    availabilityStatus: string;
+    reviews: IProductReview[],
+    returnPolicy: string;
+    minimumOrderQuantity: number;
+    meta: IProductMeta,
+    images: string[],
+    thumbnail: string;
+}
+
+export interface IProductCategory {
+    slug: string;
+    name: string;
+    url: string;
+}
